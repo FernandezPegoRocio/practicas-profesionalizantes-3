80974a5745baa23f52acc9c4d647ec767784fca6
Rocío <162938513+FernandezPegoRocio@users.noreply.github.com>
2026-04-07 17:18:25 -0300

    Update app.js

--- diagnostico/actividad-1/frontend/app.js
+++ diagnostico/actividad-1/frontend/app.js
@@ -1,1 +1,42 @@
+window.onload = mostrarStock;
 
+function mostrarStock() {
+    fetch('http://localhost:3000/stock')
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(datos) {
+        const tabla = document.getElementById('tablaStock');
+        tabla.innerHTML = '';
+        
+        datos.forEach(function(fila) {
+            tabla.innerHTML += '<tr>' +
+                '<td>' + fila.id + '</td>' +
+                '<td>' + fila.material + '</td>' +
+                '<td>' + fila.medida + '</td>' +
+                '<td>' + fila.cantidad + '</td>' +
+                '<td>' + fila.precio_unitario + '</td>' +
+                '<td>' + fila.precio_total + '</td>' +
+            '</tr>';
+        });
+    })
+    .catch(function(err) {
+        console.error('Error al cargar el stock', err);
+    });
+}
+
+function main() {
+
+   document.getElementById('btnMostrarStock').onclick = mostrarStock;
+    document.getElementById('btnAgregarMaterial').onclick = mostrarFormularioAgregar;
+    document.getElementById('btnRegistrarCompra').onclick = mostrarFormularioCompra;
+    document.getElementById('btnRegistrarVenta').onclick = mostrarFormularioVenta;
+
+  
+    if (btnMostrarStock) btnMostrarStock.onclick = mostrarStock;
+    if (btnAgregarMaterial) btnAgregarMaterial.onclick = mostrarFormularioAgregar;
+    if (btnRegistrarCompra) btnRegistrarCompra.onclick = mostrarFormularioCompra;
+    if (btnRegistrarVenta) btnRegistrarVenta.onclick = mostrarFormularioVenta;
+  mostrarStock();
+}
+window.onload = main;
